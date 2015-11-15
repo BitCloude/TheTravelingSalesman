e3c357f9b5cd0eb37b88d80536efad7d3161687b
Zoe Dai <zoedai91@gmail.com>
2015-11-15 03:34:36 -0800

    navigation added

--- app/src/main/java/com/appers/ayvaz/thetravelingsalesman/MainActivity.java
+++ app/src/main/java/com/appers/ayvaz/thetravelingsalesman/MainActivity.java
@@ -3,7 +3,12 @@ package com.appers.ayvaz.thetravelingsalesman;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
 FloatingActionButton fabAddClient;
     ListView listViewClient;
     String[] clients = {"Client 0", "Client 1", "Client 2","Client 3", "Client 4", "Client 5","Client 6", "Client 7", "Client 8","Client 9", "Client 10", "Client 11","Client 12", "Client 13", "Client 14","Client 15", "Client 16", "Client 17"};
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private NavigationView mNavigationView;
+    private FragmentManager mFragmentManager;
+    private CharSequence mTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +51,51 @@ FloatingActionButton fabAddClient;
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,clients);
         listViewClient.setAdapter(adapter);
+
+
+        // begin drawer stuff
+//        mMenuTitles = getResources().getStringArray(R.array.drawer_menu);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mTitle = mDrawerTitle = getTitle();
+        //navi view implementation
+        mNavigationView = (NavigationView) findViewById(R.id.navi_menu);
+        setupDrawerContent(mNavigationView);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                item.setChecked(true);
+                selectItem(item.getItemId());
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+//                setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+//                setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
+            }
+        };
+
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
@@ -61,5 +118,47 @@ FloatingActionButton fabAddClient;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    //todo: setup navigation
+    private void selectItem(int itemId) {
+/*        Fragment fragment = new ItemFragment();
+
+
+        switch (itemId) {
+            case R.id.nav_clients:
+                fragment = new ItemFragment();
+                break;
+            case R.id.nav_report_client:
+                fragment = new ItemFragment();
+                break;
+            case R.id.nav_report_expense:
+                fragment = new ItemFragment();
+                break;
+            case R.id.nav_report_upcoming:
+                fragment = new ItemFragment();
+                break;
+        }
+
+
+
+        mFragmentManager.beginTransaction().replace(R.id.fragmentContent, fragment)
+                .addToBackStack(null).commit();
+*/
+
     }
 }
