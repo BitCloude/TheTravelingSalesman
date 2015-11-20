b58925544dfcf797d05e0aeb8185bd8dc8303785
BitCloude <aandreasyan@ucdavis.edu>
2015-11-20 13:00:06 -0800

    11/20/2015 ExpenseAdd,Expense,Travel,TravelDetail UI

--- app/src/main/java/com/appers/ayvaz/thetravelingsalesman/Expenses.java
+++ app/src/main/java/com/appers/ayvaz/thetravelingsalesman/Expenses.java
@@ -1,18 +1,31 @@
 package com.appers.ayvaz.thetravelingsalesman;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Expenses extends AppCompatActivity {
 Spinner spinnerClient, spinnerAmount, spinnerFrom;
@@ -21,41 +34,86 @@ Spinner spinnerClient, spinnerAmount, spinnerFrom;
     Integer[] amount = {1,5,10,20,50,100,500,1000};
     String[] from= {"Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday","Sunday"};
     String[] expenses= {"Client 1:10", "Client 2: 5", "Client 3: 20","Client 4: 100", "Client 5: 400", "Client 6: 1", "Client 7: 50", "Client 8: 24", "Client 9: 70", "Client 10: 13"};
+    LinearLayout linearLayout;
+    RelativeLayout childLayout;
+    String data = "Hotel Bill From Delhi to Hyderhabad";
+    String cost = "$25,000";
+    //Trip From Delhi to Hyderhabad
+    Button expensesBut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        spinnerClient = (Spinner) findViewById(R.id.spinnerExpenseClient);
-        spinnerAmount = (Spinner) findViewById(R.id.spinnerExpenseAmount);
-        spinnerFrom = (Spinner) findViewById(R.id.spinnerExpenseFrom);
-        listViewExpenses = (ListView) findViewById(R.id.listViewExpenses);
+        listViewExpenses = new ListView(this);
+        expensesBut = (Button) findViewById(R.id.expensesExpensesButton);
+        linearLayout = (LinearLayout) findViewById(R.id.expensesLinearLayout);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,client);
-        spinnerClient.setAdapter(adapter);
-        ArrayAdapter<Integer> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,amount);
-        spinnerAmount.setAdapter(adapter2);
-        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,from);
-        spinnerFrom.setAdapter(adapter3);
-        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,expenses);
-        listViewExpenses.setAdapter(adapter4);
+        childLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams layoutParamsCost = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutParamsCost.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 
-        listViewExpenses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        RelativeLayout.LayoutParams layoutParamsData = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutParamsData.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        //childLayout.setOrientation(LinearLayout.HORIZONTAL);
+        TextView textdata = new TextView(this);
+        textdata.setLines(2);
+
+        textdata.setText(data);
+        TextView textcost = new TextView(this);
+        textcost.setLines(1);
+        textcost.setText(cost);
+
+        childLayout.setBackgroundColor(Color.LTGRAY);
+        childLayout.addView(textdata,layoutParamsData);
+        childLayout.addView(textcost,layoutParamsCost);
+
+
+
+        TextView textView = new TextView(this);
+        textView.setText("October 2015");
+        TextView textView2 = new TextView(this);
+        textView2.setText("November 2015");
+     //   ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,expenses);
+       // listViewExpenses.setAdapter(adapter4);
+
+        //childlayout.addView(textView);
+        linearLayout.addView(textView);
+        linearLayout.addView(childLayout);
+        linearLayout.addView(textView2);
+
+        registerForContextMenu(childLayout);
+
+       // linearLayout.addView(listViewExpenses);
+       /* listViewExpenses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView parent, View v, int position, long id) {
                 Intent intent = new Intent(getApplicationContext(),ExpenseAdd.class);
                 startActivity(intent);
             }
         });
-
+*/
+        expensesBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ExpenseAdd.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.add(1, 11, 100, "ADD");
+        //menu.findItem(11).setIcon(android.R.drawable.ic_menu_save);
+        menu.findItem(11).setIcon(android.R.drawable.ic_menu_add);
+        menu.findItem(11).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return true;
     }
 
@@ -72,6 +130,32 @@ Spinner spinnerClient, spinnerAmount, spinnerFrom;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        int groupId = 1;
+        menu.add(groupId, 11, 100, "Edit");
+        menu.add(groupId, 22, 200, "Delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == 11) {
+            Toast.makeText(getApplicationContext(), "Edit", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if (id == 22) {
+            Toast.makeText(getApplicationContext(), "Delete", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+
+        return super.onContextItemSelected(item);
     }
 
 }
